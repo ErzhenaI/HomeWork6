d062a05d5297834de7c839d3c20d3bc6be19e6a7
Erzhena Istomina <era155@mail.ru>
2023-06-17 18:13:00 +0800

    done Tasks 41 & 43

---
+++ Task41/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] Array = new int[M];
+
+void MassivArray(int M)
+{
+    for (int i = 0; i < M; i++)
+    {
+        Console.WriteLine($"Введите { i + 1} элемент массива: ");
+        Array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+int CountNumbers (int[] Array)
+{
+    int count = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i] > 0)
+            count ++;
+    }
+    return count;
+}
+Console.Clear();
+MassivArray(M);
+Console.Write($"Количество чисел больше нуля: {CountNumbers(Array)}");
---
+++ Task43/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите 1-е значение прямой b1b2: ");
+double b1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-е значение прямой b1b2: ");
+double b2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 1-е значение прямой k1k2: ");
+double k1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-е значение прямой k1k2: ");
+double k2 = int.Parse(Console.ReadLine()!);
+
+double x = (-(b1 - b2)) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.Write($"Точка пересечения двух прямых: [{x},{y}]");
